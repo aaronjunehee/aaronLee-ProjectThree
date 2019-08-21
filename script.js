10c6895a57df74851cb08704ab567fe0251f9ef2
Aaron <aaronjunehee@gmail.com>
2019-08-21 11:02:27 -0400

    added snack assets :doughnut:

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const snackArray = [
+    "./styles/assets/snackPizza.jpg",
+    "./styles/assets/snackFries.jpg",
+    "./styles/assets/snackBubble.jpg",
+    "./styles/assets/snackCone.jpg",
+]
+
+
+
+
+
 $(document).ready(function(){
     $('.card').on('click', function(){
         console.log('Hello');
